53c543b2f60d5dc35a5a2180798d69c38531fdb0
Jeff Adkins <Jeffadkins1@gmail.com>
2025-07-12 22:45:44 -0600

    Add version indicator to test deployment

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import EnhancedGrowGardenCalculator from './components/EnhancedGrowGardenCalcula
 function App() {
   return (
     <div className="App">
+      <div className="text-center text-sm text-gray-500 mb-2">
+        SheckleShare v1.1.0 - Advanced GAG Calculator
+      </div>
       <EnhancedGrowGardenCalculator />
     </div>
   )
